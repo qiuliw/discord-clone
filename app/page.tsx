8a2caec4626812110661eadfe849bddae8bb3751
qiuliw <a1807191473@qgmail.com>
2026-08-13 16:59:42 +0800

    feat: add login/register pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
-      {/* indigo 靛蓝色 ; 500 色阶 */}
-      <p className="test-3xl font-bold text-indigo-500">
-        Hello Discord Clone
-      </p>
-      <Button>Click me</Button>
+      This is the home page
     </div>
-  );
+  )
 }
